a5f530789d8fb46517cf2e54ed567c977ee26382
git <scotthurlbert@hotmail.com>
2016-01-23 17:18:57 -0800

    Initial check in.

---
+++ Urler/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Urler.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static string LastUrl { get; set; }
+
+        public ActionResult Index()
+        {
+            ViewBag.LastUrl = LastUrl;
+
+            return View();
+        }
+
+        public ActionResult SaveUrl( string url )
+        {
+            if( !string.IsNullOrEmpty( url ) )
+            {
+                LastUrl = url;
+            }
+            return RedirectToAction( "Index" );
+        }
+
+        public JsonResult GetUrl()
+        {
+            var result = new { Url = LastUrl };
+            return Json( result, JsonRequestBehavior.AllowGet );
+        }
+
+        public ActionResult GotoUrl()
+        {
+            if( String.IsNullOrEmpty( LastUrl ) ) { return RedirectToAction( "Index" ); }
+            return Redirect( LastUrl );
+        }
+    }
+}
---
+++ Urler/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Urler.MvcApplication" Language="C#" %>
